ad66df7a4648e6d717ed25622c75a42dbbab501c
itoma1124 <alpineski100@gmail.com>
2021-02-02 16:03:14 +0900

    いいねテーブルとコントローラの追加

--- app/models/sake.rb
+++ app/models/sake.rb
@@ -3,7 +3,7 @@ class Sake < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments,     dependent: :destroy
-  # has_many :favorites, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
