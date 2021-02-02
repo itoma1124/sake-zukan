9191450ad5d6549533c066cd651de402b84f2b99
itoma1124 <alpineski100@gmail.com>
2021-02-02 15:14:21 +0900

    コントローラーの作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create 
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to "/sakes/#{comment.sake.id}"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to root_path
+    end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text).merge(user_id: current_user.id, sake_id: params[:sake_id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   get 'users/show'
   devise_for :users
   root to: "sakes#index"
